a6a4198172d800198304e3fc0b28c7828c2f1066
Artem Kaloev <kaloev.eltech@gmail.com>
2017-06-04 15:17:10 +0300

    Добавлены комментарии к nclass.cpp

--- nclass.cpp
+++ nclass.cpp
@@ -23,40 +23,68 @@ string N::to_str()
     return t;
 }
 //=======================================================================================
+
+//Оператор "+"
 N operator + (N& a, N& b)
 {
-   //N c("123");
-    //printf("\nnew!! a size = %d, b size = %d", a.digit.size(), b.digit.size());
-
+    /*
+     * Переменной bigger пирсваивается число с большим кол-вом цифр, smaller - с меньшим
+     */
     N bigger = (a.digit.size() < b.digit.size())?b:a;
     N smaller = (a.digit.size() < b.digit.size())?a:b;
 
-   int sum = 0;
+    /*
+     * result - результирующее число
+     * sum - переменная, хранящая сумму текущих цифр
+     */
    N result;
-
-   //printf(" \nbigger size = %d, smaller size = %d\n", bigger.digit.size(), smaller.digit.size());
-
+   int sum = 0;
    for (int i = 0; i < bigger.digit.size(); i++) {
+       /*
+        * Если меньшее число не закончилось, цифры обоих чисел суммируются.
+        */
        if (i < smaller.digit.size()) {
            sum += bigger.digit[i] + smaller.digit[i];
        } else {
            sum += bigger.digit[i];
        }
+       /*
+        * Если сумма цифр меньше или равна 9, то она записывается в разряд результирующего числа,
+        * переменная sum обнуляется
+        */
        if (sum <= 9) {
            result.digit.push_back(sum);
            sum = 0;
-       } else {
-           result.digit.push_back(sum - 10);
+       }
+       /*
+        * Иначе в результирующее число записывается остаток по модулю 10,
+        * переменная sum становится равной 1
+        */
+       else {
+           result.digit.push_back(sum % 10);
            sum = 1;
+           /*
+            * Если это последний разряд числа наибольшего числа,
+            * то в дополнительный разряд результирующего числа записывается единица
+            */
            if (i == bigger.digit.size() - 1) {
                result.digit.push_back(sum);
            }
        }
    }
+   /*
+    * Возвращает результирующее число
+    */
    return result;
 }
 
-bool operator == (N& a, N& b) {
+//Оператор "=="
+bool operator == (N& a, N& b)
+{
+    /*
+     * Возвращает true, если числа равны,
+     * иначе возвращает false
+     */
     if (a.digit.size() != b.digit.size()) return false;
     for (int i = a.digit.size() - 1; i >= 0; i--) {
         if (a.digit[i] != b.digit[i]) return false;
@@ -64,7 +92,13 @@ bool operator == (N& a, N& b) {
     return true;
 }
 
-bool operator > (N& a, N& b) {
+//Оператор ">"
+bool operator > (N& a, N& b)
+{
+    /*
+     * Возвращает true, если (a > b),
+     * иначе возвращает false
+     */
     if (a.digit.size() > b.digit.size()) return true;
     if (a.digit.size() < b.digit.size()) return false;
     for (int i = a.digit.size() - 1; i >= 0; i--) {
@@ -74,25 +108,39 @@ bool operator > (N& a, N& b) {
     return false;
 }
 
-bool operator < (N& a, N& b) {
+//Оператор "<"
+bool operator < (N& a, N& b)
+{
+    /*
+     * Возвращает true, если (a < b),
+     * иначе возвращает false
+     */
     if (a.digit.size() < b.digit.size()) return true;
     if (a.digit.size() > b.digit.size()) return false;
     for (int i = a.digit.size() - 1; i >= 0; i--) {
-        //printf("a [i] = %d\nb[i] = %d\n",a.digit[i],b.digit[i]);
         if (a.digit[i] > b.digit[i]) return false;
         else if (a.digit[i] < b.digit[i]) return true;
     }
     return false;
 }
 
-N operator - (N& a, N& b) {
-    N result;
-    //по умолчанию а > b, иначе возвращает пустое N
+//Оператор "-"
+N operator - (N& a, N& b)
+{
+    /*
+     * Если уменьшаемое число меньше вычитаемого, то возвращается уменьшаемое
+     */
     if (a < b) return a;
+    /*
+     * result - результирующее число
+     * dif - переменная, хранящая разность текущих цифр
+     */
+    N result;
     int dif = 0;
-
-    int i;
-    for (i = 0; i < b.digit.size(); i++) {
+    /*
+     * Вычитание производится, пока не закончились цифры в меньшем числе
+     */
+    for (int i = 0; i < b.digit.size(); i++) {
         a.digit[i] += dif;
         if (a.digit[i] < b.digit[i] || a.digit[i] == 255) {
             a.digit[i] += 10;
@@ -102,8 +150,10 @@ N operator - (N& a, N& b) {
         }
         result.digit.push_back(a.digit[i] - b.digit[i]);
     }
-
-    for (; i < a.digit.size(); i++) {
+    /*
+     * Вычитание продолжается, пока не закончились цифры в большем числе
+     */
+    for (int i = b.digit.size(); i < a.digit.size(); i++) {
         a.digit[i] += dif;
         if (a.digit[i] == 255) {
             a.digit[i] += 10;
@@ -113,84 +163,86 @@ N operator - (N& a, N& b) {
         }
         result.digit.push_back(a.digit[i]);
     }
-
-
     /*
-    for (int i = 0; i < a.digit.size(); i++) {
-        a.digit[i] += dif;
-        if (i < b.digit.size()) {
-            printf("%d\n", i);
-            //-1???
-            if (a.digit[i] < b.digit[i] || a.digit[i] == 255) {
-                a.digit[i] += 10;
-                dif = -1;
-            } else {
-                dif = 0;
-            }
-            result.digit.push_back(a.digit[i] - b.digit[i]);
-        } else {
-            //-1??
-            if (a.digit[i] == 255) {
-                a.digit[i] += 10;
-                dif = -1;
-            } else {
-                dif = 0;
-            }
-            result.digit.push_back(a.digit[i]);
-            }
-        }
-
-*/
-
+     * Удаление незначащих нулей в результирующем числе
+     */
     for (int i = result.digit.size() - 1; i > 0; i--) {
         if (result.digit[i] != 0) return result;
         else result.digit.erase(result.digit.end() - 1);
     }
     /*
-    for (int i = 0; i < result.digit.size() ; i++) {
-        printf("res[%d] = %d\n", i, result.digit[i]);
-    }
-*/
+     * Возвращает результирующее число
+     */
     return result;
 }
 
-bool N::chk0() {
+
+
+//Метод класса N: проверка на 0
+/*
+bool N::chk0()
+{
+    /*
+     * Если число равно 0, возвращает true, иначе возвращает false
+     *//*
     N tmp("0");
-    //for (int i = 0; i < this->digit.size() ; i++) {
-      //  printf("res[%d] = %d\n", i,this->digit[i]);
-    //}
     if (*this == tmp) return true;
     else return false;
 }
+*/
 
+
+//Метод класса N: умножение на 10 в степени k
 void N::mul10k (int k)
 {
-    if (this->chk0()) {
+    /*
+     * Если исходное число равно 0, возвращает 0
+     */
+    N tmpNull("0");
+    if (*this == tmpNull) {
         return;
     }
+    /*
+     * Добавляет разряды к исходному числу
+     */
     for (int i = 0; i < k; i++) {
         this->digit.emplace(this->digit.begin(), 0);
     }
 }
 
-
+//Метод класса N: деление на натуральное длинное число
 void N::divN(N b)
 {
     N tmpNull("0");
     N a = *this, result;
-    if (a < b) {
-        *this = tmpNull;
-        return;
-    }
-    if (b.chk0()) {
+    /*
+     * Если делитель равен 0, то возвращает исходное число
+     */
+    if (b == tmpNull) {
         puts("Деление на 0: операция не определена. Возврат исходного значения . . .");
         return;
     }
+    /*
+     * k - разность в кол-вах цифр между делимым и делителем
+     * Если отрицательна, то возвращается 0
+     */
     int k = a.digit.size() - b.digit.size();
+    if (k < 0) {
+        *this = tmpNull;
+        return;
+    }
+    /*
+     * Результирующее число задается нулями (кол-во цифр в числе определяется как (k + 1) )
+     */
     for (int j = 0; j < k + 1; j++){
         result.digit.push_back(0);
     }
-
+    /*
+     * Производится деление путем вычитания из исходного числа временной переменной, равной
+     * делителю, умноженному на 10 в степени k.
+     * Если делимое становится меньше переменной, то степень k уменьшается и происходит переход к следующему разряду.
+     * Если делимое становится меньше делителя, цикл завершается
+     */
     for (int i = result.digit.size() - 1; a > b || a == b; ) {
         N tmp = b;
         tmp.mul10k(k);
@@ -202,34 +254,33 @@ void N::divN(N b)
         a = a - tmp;
         result.digit[i] += 1;
     }
-
+    /*
+     * Удаление незначащих нулей в результирующем числе
+     */
     for (int i = result.digit.size() - 1; i > 0; i--) {
         if (result.digit[i] != 0) break;
         else result.digit.erase(result.digit.end() - 1);
     }
-
+    /*
+     * Возвращает результирующее число
+     */
     *this = result;
 }
 
+//Оператор "%"
 N operator % (N& a, N& b)
 {
-
+    /*
+     * Находит целую часть от деления чисел, умножает результат на исходный делитель, получившееся число
+     * вычитает из исходного делимого и возвращает
+     */
     N tmp = a;
-    //for (int i = 0; i < tmp.digit.size() ; i++) {
-      //  printf("tmp[%d] = %d\n", i,tmp.digit[i]);
-    //}
     tmp.divN(b);
-    //or (int i = 0; i < tmp.digit.size() ; i++) {
-      //  printf("tmp[%d] = %d\n", i,tmp.digit[i]);
-    //}
     tmp.mulN(b);
-    //tmp = a - tmp;
-
-    //a = a - tmp;
-
     return a - tmp;
 }
 
+//Метод класса N: перевод в двоичную строку
 string N::to_binstr()
 {
     string t, nstr;
@@ -240,20 +291,31 @@ string N::to_binstr()
         tmp.divN(odd);
     }
     t += tmp.digit[0] + '0';
+    /*
+     * Символы в строке хранятся в обратном порядке, т.е. если число не 0, первый символ всегда '1'
+     */
     for (int i = t.length() - 1; i >= 0; i--) {
         nstr += t[i];
     }
     return nstr;
 }
 
+//Метод класса N: возведение в степень по модулю
 void N::powmod(N pow, N mod)
 {
-    N odd("2"), tmpOne("1"), tmpThis = *this;
-
+    N tmpOne("1"), tmpThis = *this;
+    /*
+     * Если степень равна 1, то производится деление по модулю и метод завершает своб работу
+     */
     if (pow == tmpOne) {
         (*this) = (*this) % mod;
         return;
     }
+    /*
+     * Для возведения числа в степень применяется алгоритм бинарного возведения.
+     * Каждый шаг возведения сопровождается делением по модулю, для того
+     * чтобы уменьшить длительность операции возведения
+     */
     string powStr = pow.to_binstr();
     for (int i = 1; i < powStr.length(); i++) {
         this->mulN(*this);
@@ -264,7 +326,7 @@ void N::powmod(N pow, N mod)
     }
 }
 
-
+//Метод класса N: умножение на цифру
 void N::mulK(int k)
 {
     N tmpNull("0");
